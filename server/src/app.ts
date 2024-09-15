d57e2c9bbf9098c28989f703e67b7a73a0021237
Александр Угаров <augarov@web-bee.ru>
2024-09-15 13:39:29 +0300

    init:(AV-01): создан фронт и бэк

---
+++ server/src/app.ts
@@ -0,0 +1,23 @@
+import Koa from 'koa';
+import bodyParser from 'koa-bodyparser';
+import cors from '@koa/cors';
+import helmet from 'koa-helmet';
+import json from 'koa-json';
+import logger from 'koa-logger';
+import 'reflect-metadata';
+import router from './server';
+
+const app = new Koa();
+const port = process.env.PORT || 3001;
+
+app.use(helmet());
+app.use(cors());
+app.use(json());
+app.use(logger());
+app.use(bodyParser());
+
+app.use(router.routes()).use(router.allowedMethods());
+
+app.listen(port, () => {
+  console.log(`🚀 App listening on the port ${port}`);
+});
